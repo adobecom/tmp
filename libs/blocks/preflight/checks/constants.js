2ef47e3d0195d82ae4f2d437534982fef8822b07
skholkhojaev <skholkhojaev@adobe.com>
2025-09-09 14:32:01 +0200

    rebased and fixed conflicts

--- libs/blocks/preflight/checks/constants.js
+++ libs/blocks/preflight/checks/constants.js
@@ -73,12 +73,12 @@ export const SEO_DESCRIPTIONS = {
 export const CHECKS = {
   H1_COUNT: {
     id: 'h1-count',
-    severity: SEVERITY.WARNING,
+    severity: SEVERITY.CRITICAL,
     title: 'H1 count',
   },
   TITLE_SIZE: {
     id: 'title-size',
-    severity: SEVERITY.WARNING,
+    severity: SEVERITY.CRITICAL,
     title: 'Title size',
   },
   CANONICAL: {
@@ -88,12 +88,12 @@ export const CHECKS = {
   },
   META_DESCRIPTION: {
     id: 'meta-description',
-    severity: SEVERITY.WARNING,
+    severity: SEVERITY.CRITICAL,
     title: 'Meta description',
   },
   BODY_SIZE: {
     id: 'body-size',
-    severity: SEVERITY.WARNING,
+    severity: SEVERITY.CRITICAL,
     title: 'Body size',
   },
   LOREM_IPSUM: {
